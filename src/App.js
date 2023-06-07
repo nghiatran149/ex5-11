6c40b2cfffc76ee194065700773bf12a10616231
nghiatran149 <mrnghia02@gmail.com>
2023-06-07 15:39:22 +0700

    ex9

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navigation from './Components/Navigation';
 import Player from './Components/Player';
 import Footer from './Components/Footer';
+import Main from './Components/Main';
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      <Player/>
+      {/* <Player/> */}
+      <Main/>
       <Footer/>
     </div>
   );
